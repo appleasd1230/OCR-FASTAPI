d489dc13d7fc767d3b1e8aa15a9670bfb0cb3f9d
Josh <appleasd1230@gms.tku.edu.tw>
2023-01-31 10:24:20 +0800

    add demo folder and demo file

--- app/main.py
+++ app/main.py
@@ -82,12 +82,12 @@ app.add_middleware(
 
 app.doc_ocr = PaddleOCR(
     det_model_dir=r'ml_models/ch_PP-OCRv3/ch_PP-OCRv3_det_infer/',
-    rec_model_dir=r'ml_models/ppocv_server_v2.0/ch_ppocr_server_v2.0_rec_infer/',
+    rec_model_dir=r'ml_models/ppocr_server_v2.0/ch_ppocr_server_v2.0_rec_infer/',
     # det_model_dir=r'app/ml_models/ch_PP-OCRv3/ch_PP-OCRv3_det_infer/',
     # rec_model_dir=r'app/ml_models/ch_PP-OCRv3/ch_PP-OCRv3_rec_infer/',
     rec_char_dict_path=r'dict/cathay_dict.txt',
     cls_model_dir=r'ml_models/ppocr_mobile_v2.0/ch_ppocr_mobile_v2.0_cls_infer/',
-    use_gpu=False,
+    use_gpu=True,
     use_angle_cls=True,
     lang="ch",
     enable_mkldnn=True,
@@ -309,4 +309,4 @@ wsgi_app = ASGIMiddleware(app)
 
 # IIS 上的版本底下兩行要拿掉
 if __name__ == "__main__":
-    uvicorn.run('main:app', workers=2)
+    uvicorn.run('main:app', workers=2, reload=True)
